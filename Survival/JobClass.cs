ceb932e226a3bcb50151b40ab6040782bea49155
Yadermen <yaroslawshemet@gmail.com>
2024-07-31 11:42:44 +0200

    Update and rename Class2.cs to JobClass.cs

---
+++ Survival/JobClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Works
+{
+    public class Job
+    {
+
+       
+
+        public Job(string jobName, float salary, float expSalary, float jobPrice, float expPrice)
+        {
+            JobName = jobName;
+            Salary = salary;
+            ExpSalary = expSalary;
+            JobPrice = jobPrice;
+            ExpPrice = expPrice;
+        }
+        public string JobName { get; private set; }
+        public float Salary { get; private set; }
+        public float ExpSalary { get; private set; }
+        public float JobPrice { get; private set; }
+        public float ExpPrice { get; private set; }
+    }
+
+
+
+
+}
+}
