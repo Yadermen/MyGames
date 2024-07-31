dfba641e3f45d75338f2478205032261a8656216
Yadermen <yaroslawshemet@gmail.com>
2024-07-31 11:43:50 +0200

    Update and rename Class1.cs to ItemClass.cs

---
+++ Survival/ItemClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+
+namespace Items
+{
+    public class Item
+    {
+      
+    
+        
+        public Item(string itemName, int itemPrice, float factor)
+        {
+            ItemName = itemName;
+            ItemPrice = itemPrice;
+            Factor = factor;
+        }
+        public string ItemName { get; private set; }
+        public int ItemPrice { get; private set; }
+        public float Factor { get; private set; }
+    }
+}
+
